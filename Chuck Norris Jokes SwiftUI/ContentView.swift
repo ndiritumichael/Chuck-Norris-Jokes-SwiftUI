a468eb22622dcd7814637743c38c274379342f17
Michael <ndiritumichael5@gmail.com>
2022-02-24 23:57:39 +0300

    Rest Apis in Swift

--- Chuck Norris Jokes SwiftUI/ContentView.swift
+++ Chuck Norris Jokes SwiftUI/ContentView.swift
@@ -8,9 +8,39 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var jokes : [Joke] = []
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        List{
+            if jokes.isEmpty{
+                Text("No Jokes Loaded Yet")
+            }
+            
+            ForEach(jokes){joke in
+                Text(joke.value)
+                
+            }
+            
+            
+            Button(action: {Task {
+                let (data, _) = try await URLSession.shared.data(from: URL(string:"https://api.chucknorris.io/jokes/random")!)
+                let decodedResponse = try? JSONDecoder().decode(Joke.self, from: data)
+              let   joke = decodedResponse!
+                jokes.append(joke)
+            }}){
+                
+                
+                Label("Fetch Joke",systemImage: "command.circle.fill")
+            }.labelStyle(.titleAndIcon)
+                .buttonStyle(.bordered)
+                
+    }
+    }
+    
+    struct Joke: Codable,Identifiable {
+        let id = UUID()
+        let value: String
     }
 }
 
