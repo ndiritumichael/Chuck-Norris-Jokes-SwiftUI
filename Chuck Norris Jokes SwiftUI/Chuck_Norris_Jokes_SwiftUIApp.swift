228088d88b336c01326a56edb4ecaaeb9966fd17
Michael <ndiritumichael5@gmail.com>
2022-02-24 22:33:35 +0300

    Initial Commit

---
+++ Chuck Norris Jokes SwiftUI/Chuck_Norris_Jokes_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Chuck_Norris_Jokes_SwiftUIApp.swift
+//  Chuck Norris Jokes SwiftUI
+//
+//  Created by mac on 24/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct Chuck_Norris_Jokes_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Chuck Norris Jokes SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Chuck Norris Jokes SwiftUI
+//
+//  Created by mac on 24/02/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
